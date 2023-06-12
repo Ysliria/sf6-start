7d82f44fad9f680a473b8038adae82a71e0f5046
Mickaël AUGER <auger.mickael37@gmail.com>
2023-06-12 12:18:36 +0200

    [BUGFIX] Correction pour les noms composés

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/hello', name: 'hello_world_')]
 class HelloWorldController extends AbstractController
 {
-    #[Route('/{name}', name: 'index', requirements: ['name' => "\w+"], methods: ['GET'], defaults: ['name' => 'world'])]
+    #[Route('/{name}', name: 'index', requirements: ['name' => "\w([-\w])*"], defaults: ['name' => 'world'], methods: ['GET'])]
     public function index($name): Response
     {
         return new Response("Hello $name!");
