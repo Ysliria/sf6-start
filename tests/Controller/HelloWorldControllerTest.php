ab88a8dd1488336ff15e1c306a5dc0f5fbe49858
Mickaël AUGER <auger.mickael37@gmail.com>
2023-06-13 17:11:48 +0200

    [TEST] Refacto & provider

---
+++ tests/Controller/HelloWorldControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HelloWorldControllerTest extends WebTestCase
+{
+    public static function provideValidURLs(): \Generator
+    {
+        yield 'default' => [
+            'uri' => '/hello'
+        ];
+
+        yield 'with name Mickaël' => [
+            'uri' => '/hello/Mickaël'
+        ];
+
+        yield 'with name Jean-Lou' => [
+            'uri' => '/hello/Jean-Lou'
+        ];
+    }
+
+
+    /**
+     * @group type/smoke
+     *
+     * @dataProvider provideValidURLs
+     */
+    public function testItWorks(string $uri): void
+    {
+        $client = static::createClient();
+        $client->request('GET', $uri);
+
+        self::assertResponseIsSuccessful();
+    }
+}
